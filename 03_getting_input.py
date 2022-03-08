86ffc178feee85d41954097b0e060ed2aa59c53d
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-09 10:59:53 +1300

    Add files via upload
    input

---
+++ 03_getting_input.py
@@ -0,0 +1,11 @@
+# ask the user for their name                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             # Ask he user for their name
+username = input("what is your name? ")
+
+# Ask the user for their favourite integer
+fav_num = int(input("favourte number?"))
+# Double, halve and square the number
+
+# Greet the user
+
+# output the results of doubling, halving
+# and squaring their favourite integer
