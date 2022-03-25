90b1839fa56adcbac1ac44c22c4afedade217666
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-25 14:26:25 +1300

    Add files via upload

---
+++ 05_break continue.py
@@ -0,0 +1,17 @@
+print()
+Print("*****looping demo******" )
+print()
+
+for item in range(0,5 ):
+    print(item)
+    
+ # ask the user if they ant to keep going...
+ keep_going = input("enter to keep looping, or any key to quiet")   
+ 
+ # end loop if user presses enter
+        if keep_going != "":
+            break
+        
+ print()   
+ print("we are done")    
+print()
