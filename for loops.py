8eed55a1aff4b892eeae2fbf20a8fd4c3e2dbcc1
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-29 11:41:17 +1300

    Add files via upload

--- for loops.py
+++ for loops.py
@@ -3,9 +3,9 @@ print("Looping Demo")
 
 
 # Quick for loop
-for item in range (1,10):
-     Print(item)
+for item in range (10, -1, -1):
+     print(item)
      
- print()
- print("End of program") 
- print()   
+print()
+print("End of program") 
+print()   
