1bfac696a77a2eaa30a2dbda1aa207766d194279
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-29 12:15:34 +1300

    Add files via upload

--- coffee_order.py
+++ coffee_order.py
@@ -2,6 +2,18 @@ print()
 print("Coffee order demo")
 
 keep_going = ""
-while keep_going == ""
-
+while keep_going == "":
+    
+    want_coffee = input("Do you want hot chocolate?")
+    if want_coffee != "yes":
+        print("wrong answer, you always want hot chocolate")
+        continue
+    
+    else:
+        print("Good choice")
+        break
+    
+print()
+print("End of program")
+print()
     
