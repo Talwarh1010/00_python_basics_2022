0330f1d7e2cd537791d3e215488edecd452e0631
Talwarh1010 <100240731+Talwarh1010@users.noreply.github.com>
2022-03-29 15:47:33 +1300

    Add files via upload
    final fixes

--- coffee_order.py
+++ coffee_order.py
@@ -4,7 +4,7 @@ print("Coffee order demo")
 keep_going = ""
 while keep_going == "":
     
-    want_coffee = input("Do you want hot chocolate?")
+    want_coffee = input("Do you want hot chocolate?").lower()
     if want_coffee != "yes":
         print("wrong answer, you always want hot chocolate")
         continue
